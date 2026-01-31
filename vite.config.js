e3b0e74f2763e8981044a332c3a400400c94f1bc
IITJ <iitj@Manthans-MacBook.local>
2026-01-31 16:37:52 +0530

    increse chunk size to 1.6mb

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600, // Increases the limit to 1.6MB
+  },
 })
